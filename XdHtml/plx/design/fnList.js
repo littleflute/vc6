ddb18f5ce12ffb06037317463a8bbc911a3f3f2a
littleflute <13699175164@sina.cn>
2017-12-20 06:14:18 +0800

    Update fnList.js

--- XdHtml/plx/design/fnList.js
+++ XdHtml/plx/design/fnList.js
@@ -9,7 +9,8 @@ var ls = [
 ];
 var ld = bl$("id_div_function_list");
 if(ld){
-	ld.innerHTML = "v0.0.16 ";
+	ld.innerHTML = "v0.0.17 ";
+	var lv = blo0.blDiv(ld,ld.id+"_lv_","lv",blGrey[5]);
 	for(i in ls){
 		var b = blo0.blBtn(ld,ld.id+"_btn_"+i,"f"+i,blGrey[0]);
 		b.onclick = function(n_){
@@ -25,6 +26,7 @@ if(ld){
 					if(this.blFunListPath){
 						s =  this.blFunListPath + s;
 					} 
+					lv.innerHTML = s;
 
 					blo0.blScript(idScript,s);
 				}
