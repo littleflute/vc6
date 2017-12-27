409ed268e63d1b5b3a0d6d90ea4d7c30f2ed4d8b
littleflute <13699175164@sina.cn>
2017-12-27 15:51:49 +0800

    Create xd2.js

---
+++ XdHtml/cchess/pu/xd2.js
@@ -0,0 +1,14 @@
+var xd2 = function(){
+  //xd2.js * 
+
+  var _v			= "v0.0.55"; 
+  var title			= "Littleflute[业6-1] 先胜 悟道[业6-1]"; 
+var s = "H2-D2+H7-E7+H0-G2+H9-G7+I0-H0+G6-G5+C3-C4+B9-A7+B0-C2+B7-B3+C2-D4+B3-G3+A0-B0+A9-B9+B2-B6+D9-E8+G0-E2+I6-I5+A3-A4+I9-H9+H0-H9+G7-H9+D4-E6+B9-B7+E6-G5+H9-G7+G5-E4+G3-H3+E4-F6+E7-F7+C4-C5+C6-C5+B6-E6+C9-E7+B0-B7+F7-B7+F6-G8+E9-D9+E6-D6+";
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.xd2 = xd2;
+	ta.value = ta.xd2;
+}
