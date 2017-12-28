06777cc3659138a84130383aa6cf0ddb8cf21361
littleflute <13699175164@sina.cn>
2017-12-28 14:19:57 +0800

    Create xd3.js

---
+++ XdHtml/cchess/pu/xd3.js
@@ -0,0 +1,15 @@
+var xd3 = function(){
+  //xd3.js * 
+
+  var _v			= "v0.0.55"; 
+  var title			= "上帝[业6-1] 先负 Littleflute[业6-1]"; 
+  var time	= "2017/12/26 20:49";
+var s = "H2-E2+B9-C7+H0-G2+A9-A8+I0-H0+H7-G7+B2-B6+G9-E7+B6-E6+C7-E6+E2-E6+D9-E8+H0-H8+G7-G3+C0-E2+A8-D8+C3-C4+D8-D6+E6-E4+B7-B8+H8-H3+B8-B3+H3-H8+D6-F6+E4-I4+B3-B8+H8-H3+G3-G0+E2-G0+H9-I7+B0-C2+F6-F2+C2-E1+G6-G5+A0-B0+B8-B2+H3-F3+B2-E2+G0-E2+F2-F3+G2-H4+F3-I3+I4-I7+I9-I7+E1-G2+I3-G3+G2-E1+G3-E3+B0-B2+I7-F7+E1-G2+E3-G3+E2-G0+I6-I5+B2-E2+G5-G4+H4-I2+G3-A3+G2-E3+F7-F3+E3-D5+A3-D3+E2-E5+I5-I4+D0-E1+E9-D9+I2-G1+F3-F4+G1-E2+F4-C4+G0-I2+C4-C5+D5-C7+D9-E9+E5-E4+D3-D7+C7-A6+C5-A5+A6-B8+D7-B7+E4-G4+I4-I3+G4-G6+C6-C5+";
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.xd3 = xd3;
+	ta.value = ta.xd3;
+}
