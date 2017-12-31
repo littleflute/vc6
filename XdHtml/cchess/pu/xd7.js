a693e1c04a5dd8af17e8c7927a18115fd71493ee
littleflute <13699175164@sina.cn>
2017-12-31 13:20:14 +0800

    Create xd7.js

---
+++ XdHtml/cchess/pu/xd7.js
@@ -0,0 +1,17 @@
+var xd7 = function(){
+  //xd7.js * 
+
+  var _v			= "v0.0.61"; 
+  var title			= "Littleflute[业6-2] 先胜 陈记文...[业6-3]  "; 
+  var time	= "2017/12/27 11:13";
+ 
+ 
+var s = "H2-D2+H7-E7+H0-G2+H9-G7+I0-H0+I9-I8+D0-E1+I8-D8+H0-H4+B9-C7+H4-C4+A9-A7+C4-C6+E6-E5+C0-E2+D8-D7+D2-D0+E7-E8+B2-D2+D7-F7+C6-D6+B7-B9+C3-C4+A7-B7+B0-C2+E5-E4+E3-E4+F7-F3+C2-D4+F3-G3+E4-E5+";
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.xd7 = xd7;
+	ta.value = ta.xd7;
+}
