bde3a56cbe9978b64cdd159771460579a90437dd
littleflute <13699175164@sina.cn>
2017-12-31 12:45:11 +0800

    Create xd4.js

---
+++ XdHtml/cchess/pu/xd4.js
@@ -0,0 +1,17 @@
+var xd4 = function(){
+  //xd4.js * 
+
+  var _v			= "v0.0.58"; 
+  var title			= "舍得 简...[业6-1] 先负 Littleflute[业6-1]"; 
+  var time	= "2017/12/26 21:20";
+ 
+var s = "H2-E2+B9-C7+H0-G2+A9-A8+I0-H0+H7-G7+C3-C4+G9-E7+B0-C2+A8-F8+C2-D4+F9-E8+G3-G4+G7-G4+G0-I2+G4-C4+B2-C2+C6-C5+A0-B0+B7-A7+D0-E1+C4-C3+H0-H4+H9-G7+E2-F2+I9-H9+H4-F4+F8-F4+G2-F4+H9-H4+D4-F5+H4-F4+F5-G7+C7-D5+G7-E6+F4-F6+E6-D4+F6-F4+D4-B5+C3-I3+C0-E2+D5-E3+B5-A7+C9-A7+B0-B7+A7-C9+B7-B6+E3-C2+F2-C2+I3-E3+B6-G6+F4-C4+C2-D2+C4-C0+D2-D0+I6-I5+G6-A6+C5-C4+A6-E6+C0-C3+A3-A4+C4-D4+E6-I6+C3-C5+I2-G0+D4-D3+I6-D6+C5-C3+D6-D4+C3-A3+G0-I2+D3-C3+I2-G0+C3-C2+D0-B0+A3-B3+B0-D0+C2-C1+A4-A5+I5-I4+D4-I4+C1-D1+I4-D4+B3-B1+D0-C0+B1-C1+C0-D0+E9-F9+D4-F4+F9-E9+F4-D4+E3-E1+A5-B5+E1-H1+D4-H4+C1-B1+B5-B6+H1-F1+H4-F4+B1-B6+";
+
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.xd4 = xd4;
+	ta.value = ta.xd4;
+}
