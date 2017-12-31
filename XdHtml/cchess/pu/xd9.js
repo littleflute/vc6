21fe9d0d9cfb1bb44bb8452696b1712d008df343
littleflute <13699175164@sina.cn>
2017-12-31 13:42:46 +0800

    Create xd9.js

---
+++ XdHtml/cchess/pu/xd9.js
@@ -0,0 +1,17 @@
+var xd9 = function(){
+  //xd9.js * 
+
+  var _v			= "v0.0.65"; 
+  var title			= "Littleflute[业6-2]   先胜 只为你而...[业6-2] "; 
+  var time	= "2017/12/27 11:51";
+ 
+ 
+var s = "H2-D2+H9-G7+H0-G2+I9-H9+I0-H0+H7-H3+C3-C4+G6-G5+B0-C2+C9-E7+C2-D4+D9-E8+D4-C6+B9-C7+B2-B6+A9-D9+A0-A2+D9-D5+A2-C2+D5-B5+D2-D6+B5-B2+C2-B2+B7-B2+C4-C5+I6-I5+H0-H1+B2-F2+B6-B7+F2-F5+D6-A6+H3-E3+H1-H9+";
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.xd9 = xd9;
+	ta.value = ta.xd9;
+}
