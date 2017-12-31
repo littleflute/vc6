1deb3b43d10c65848ce930040ea08349ba53ec2c
littleflute <13699175164@sina.cn>
2017-12-31 13:53:03 +0800

    Create xd10.js

---
+++ XdHtml/cchess/pu/xd10.js
@@ -0,0 +1,17 @@
+var xd10 = function(){
+  //xd10.js * 
+
+  var _v			= "v0.0.66"; 
+  var title			= "曹小明河...[业6-3]  先负  Littleflute[业6-2]  "; 
+  var time	= "2017/12/27 12:8";
+ 
+ 
+var s = "H2-E2+B9-C7+H0-G2+A9-A8+I0-H0+H7-G7+B2-D2+G9-E7+B0-C2+A8-F8+A0-B0+B7-A7+H0-H4+G7-G3+G0-I2+H9-G7+B0-B7+G3-C3+C0-A2+C6-C5+H4-F4+I9-I8+E2-F2+F8-F4+G2-F4+G6-G5+D2-E2+I8-H8+D0-E1+H8-H4+F4-G6+D9-E8+G6-E7+C9-E7+B7-A7+G7-F5+A7-B7+C5-C4+A2-C4+H4-C4+E3-E4+F5-D6+B7-B3+C4-C5+C2-E3+C3-I3+B3-B6+D6-B5+E4-E5+I3-A3+E5-E6+A3-B3+F2-H2+B3-B6+E6-E7+B6-H6+E7-D7+B5-C3+E2-C2+C3-E2+C2-C7+E8-D7+";
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.xd10 = xd10;
+	ta.value = ta.xd10;
+}
