c8aa6e081e5c0ab5d111fc11107d8a50cb1b36fd
littleflute <13699175164@sina.cn>
2017-12-31 13:05:49 +0800

    Update xd1.js

--- XdHtml/cchess/pu/xd1.js
+++ XdHtml/cchess/pu/xd1.js
@@ -1,8 +1,11 @@
 var xd1 = function(){
   //xd1.js * 
 
-  var _v			= "v0.0.54"; 
+  var _v			= "v0.0.55"; 
   var title			= "常青树人[业6-1] 先负 Littleflute[业6-1]";
+
+  var time	= "2017/12/26 22:52";
+ 
 var s = "H2-D2+H7-E7+H0-G2+H9-G7+I0-H0+I9-I8+H0-H4+I8-D8+F0-E1+B9-C7+B2-B4+E6-E5+B4-D4+D8-F8+B0-A2+A9-B9+D4-B4+B7-A7+D2-B2+B9-A9+B4-G4+C7-E6+G4-G7+A7-G7+A3-A4+E5-E4+B2-E2+E6-F4+E2-E4+D9-E8+G2-I1+A9-B9+A0-B0+B9-B0+A2-B0+G7-G3+B0-A2+E9-D9+A2-B4+E7-E3+G0-E2+F8-F5+B4-C2+E3-D3+H4-G4+G3-C3+G4-G6+F5-D5+G6-G4+D3-D1+C2-E3+F4-E2+E4-E2+D5-D3+E3-F5+D1-C1+C0-A2+C1-B1+E2-D2+D9-E9+G4-C4+B1-I1+C4-D4+D3-F3+F5-D6+I1-I0+D2-E2+G9-E7+E2-E6+C6-C5+A2-C0+F3-H3+E1-F2+H3-E3+";
   return s;
 };
