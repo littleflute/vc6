b905ebe94da55368c4a0d56104543c2339e17154
littleflute <13699175164@sina.cn>
2017-12-31 13:01:36 +0800

    Create xd5.js

---
+++ XdHtml/cchess/pu/xd5.js
@@ -0,0 +1,17 @@
+var xd5 = function(){
+  //xd5.js * 
+
+  var _v			= "v0.0.59"; 
+  var title			= "Littleflute[业6-1] 先胜...游客R02...[业6-1]  "; 
+  var time	= "2017/12/26 21:45";
+ 
+ 
+var s = "H2-D2+H7-E7+H0-G2+H9-G7+I0-H0+B7-D7+B0-C2+B9-C7+A0-B0+A9-B9+B2-B6+D9-E8+C3-C4+G6-G5+C0-E2+I6-I5+G3-G4+G5-G4+E2-G4+I5-I4+G4-E2+I4-I3+G2-F4+I9-I5+D0-E1+C6-C5+C2-D4+D7-D2+E1-D2+C5-C4+E2-C4+E7-E3+H0-H6+I5-F5+D4-F5+G7-F5+H6-I6+E3-E4+E0-D0+C9-E7+F0-E1+F5-D4+B0-B2+C7-D5+I6-E6+D5-F4+E6-E4+F4-E6+G0-E2+B9-B7+D0-E0+I3-H3+B2-B3+H3-G3+E4-E5+E6-G5+E5-B5+G5-F3+B6-G6+B7-A7+B5-D5+A7-A9+B3-E3+D4-C2+E3-C3+C2-A3+C3-A3+A6-A5+A3-A5+A9-B9+A5-B5+B9-A9+E2-C0+G3-G2+D5-D3+F3-H4+B5-H5+A9-A6+G6-G4+H4-I2+H5-D5+A6-E6+D3-A3+E8-D9+C4-E2+E6-E4+G4-G8+F9-E8+A3-I3+I2-G1+E0-D0+G2-H2+G8-I8+E4-H4+I8-I9+E8-F9+I9-F9+E9-F9+D5-D9+F9-F8+I3-I8+F8-F7+D9-F9+";
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.xd5 = xd5;
+	ta.value = ta.xd5;
+}
