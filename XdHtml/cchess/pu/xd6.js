a5cb2d27fbe806f60451731fa4adead1a720745e
littleflute <13699175164@sina.cn>
2017-12-31 13:12:29 +0800

    Create xd6.js

---
+++ XdHtml/cchess/pu/xd6.js
@@ -0,0 +1,17 @@
+var xd6 = function(){
+  //xd6.js * 
+
+  var _v			= "v0.0.60"; 
+  var title			= "Littleflute[业6-1] 先胜 兴龙...[业6-1]  "; 
+  var time	= "2017/12/27 11:7";
+ 
+ 
+var s = "H2-D2+C9-E7+H0-G2+H7-G7+I0-H0+H9-I7+C3-C4+B9-D8+B0-C2+A9-C9+C2-D4+D8-F7+B2-C2+D9-E8+A0-B0+F7-E5+E3-E4+E5-C4+D2-D1+B7-C7+F0-E1+C4-A3+B0-B3+C9-B9+B3-B9+C7-C9+";
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.xd6 = xd6;
+	ta.value = ta.xd6;
+}
