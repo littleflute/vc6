5e2541084dad63917f661059048614dc5d4c6c6b
littleflute <13699175164@sina.cn>
2018-01-07 10:50:48 +0800

    Update f2.js

--- XdHtml/plx/design/f2.js
+++ XdHtml/plx/design/f2.js
@@ -1,22 +1,20 @@
 var f2 = function(b,d){
 	//f2.js music staff
-  var _v			= "v0.0.69";  
+  var _v			= "v0.0.76";  
 
   var _ui = new _UIClass;
-  function _jgClass(id){
-	var _jg = new jsGraphics(id);
-	var x=0,y=0,w=600,h=400;
-	function _drawAll(){
-		_jg.clear();
-		_jg.setColor("red");
-		_jg.fillRect(x,y,w,h);
-		_jg.paint();
-	}   
-	this.upate	= function()			{ _drawAll(); }   
-	this.set	= function(x1,y1,w1,h1) { x=x1;y=y1;w=w1;h=h1; _drawAll(); }   
-  }
   function _UIClass(){   
 	var _myWrap		= null;
+    this.blrSet0	= function(b,d)    {
+		var tb = blo0.blDiv(d,"id_div_toolbar_function_f2","ld",blGrey[5]);
+		b.style.background = "gray";
+		var b1 = blo0.blBtn(tb,"id_btn_f2_toolbar_b1","b1",blGrey[0]);	
+		b1.onclick = function(){ if(_myWrap!=null) {		_myWrap.jg.set(0,40,400,300);	} }	
+		var b2 = blo0.blBtn(tb,"id_btn_f2_toolbar_b2","b2",blGrey[0]);	
+		b2.onclick = function(){ if(_myWrap!=null) {		_myWrap.jg.set(0,40,600,400);	} }	
+		var b3 = blo0.blBtn(tb,"id_btn_f2_toolbar_b3","b3",blGrey[0]);	
+		b3.onclick = function(){ if(_myWrap!=null) {		_myWrap.jg.set(0,40,800,600);	} }	
+	}  
     this.blrView	= function(b,d)
     { 
 		if(_myWrap==null){		
@@ -27,21 +25,26 @@ var f2 = function(b,d){
 		}
 	  _on_off_div(b,_myWrap);
     }
-    this.blrSet1	= function(b,d)    { 	if(_myWrap!=null) {		_myWrap.jg.set(0,20,400,300);	}   }
-    this.blrSet2	= function(b,d)    { 	if(_myWrap!=null) {		_myWrap.jg.set(0,30,600,400);	}   }
-    this.blrSet3	= function(b,d)    { 	if(_myWrap!=null) {		_myWrap.jg.set(0,40,800,600);	}   }
-    this.blrAboutMe = function(b,d){
-	  var s = _v;
-	  var LastSrcHref = "https://littleflute.github.io/vc6/XdHtml/plx/design/f2.js";
-	  var UpdateHref = "https://github.com/littleflute/vc6/edit/master/XdHtml/plx/design/f2.js";
-	  s += "<br>music staff<br>"; 
-	  var style = "color:lightblue;";
-	  var target = "_blank";
-	  s += blo0.blhMakeLink(" f2.js ",LastSrcHref,style,target);
-	  s += blo0.blhMakeLink(" f2.js* ",UpdateHref,style,target);
+    this.blr_AboutMe = function(b,d){
+	  var s = _v; 
+	  s += "<br>music staff<br>";  
+	  s += blo0.blhMakeLink(" f2.js ","https://littleflute.github.io/vc6/XdHtml/plx/design/f2.js","color:lightblue;","_blank");
+	  s += blo0.blhMakeLink(" f2.js* ","https://github.com/littleflute/vc6/edit/master/XdHtml/plx/design/f2.js","color:lightblue;","_blank");
       d.innerHTML = s;  
 	  _on_off_div(b,d);
     };
+  }
+  function _jgClass(id){
+	var _jg = new jsGraphics(id);
+	var x=0,y=0,w=600,h=400;
+	function _drawAll(){
+		_jg.clear();
+		_jg.setColor("gray");
+		_jg.fillRect(x,y,w,h);
+		_jg.paint();
+	}   
+	this.upate	= function()			{ _drawAll(); }   
+	this.set	= function(x1,y1,w1,h1) { x=x1;y=y1;w=w1;h=h1; _drawAll(); }   
   }
    
   var _myUI = function(b,d){ 
@@ -49,6 +52,8 @@ var f2 = function(b,d){
 	  if(!_myView.blFirst){
 		blo0.blShowObj2Div(_myView,_ui);
 		_myView.blFirst = true;
+		blr_AboutMe.click();
+		blrSet0.click();
 	  }
 	  _on_off_div(b,d);
   };
