bdae85df931284879f9bf11f43850b6abb406b06
littleflute <13699175164@sina.cn>
2018-01-07 13:26:42 +0800

    Update blclass.js

--- XdHtml/blclass.js
+++ XdHtml/blclass.js
@@ -1,484 +1,83 @@
-// file: blclass.js    by littleflute 
+var f2 = function(b,d){
+	//f2.js music staff
+  var _v			= "v0.0.95";  
 
-function bl$(id){	return document.getElementById(id); }
-var QueryString = function () 
-{
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
-    }
-  } 
-  return query_string;
-}(); 
+  var _ui = new _UIClass;
+  function _UIClass(){   
+	var w		= null;
+    this.blrSet0	= function(b,d)    {		
+		var tb = blo0.blDiv(d,"id_div_toolbar_function_f2_blrSet0","",blGrey[5]);
+		b.style.background = "gray";
+		var _id = "id_btn_f2_toolbar_blrSet0_b";
 
-var blColor	= ["red","tomato","gold","black","green","blue","lightblue","yellow","brown","pink","gray","white","mediumpurple",
-	"cyan"];
-var blGrey	= ["Gainsboro","LightGray","Silver","DarkGray",
-			   "DimGray","Gray","LightSlateGray","SlateGray","DarkSlateGray","black"];
+		var b1 = blo0.blBtn(tb,_id+1,"b1",blGrey[0]);b1.onclick = function(){ if(w!=null) {w.jg.set(0,40,400,300);	} }	
+		var b2 = blo0.blBtn(tb,_id+2,"b2",blGrey[0]);b2.onclick = function(){ if(w!=null) {w.jg.set(0,40,600,400);	} }	
+		var b3 = blo0.blBtn(tb,_id+3,"b3",blGrey[0]);b3.onclick = function(){ if(w!=null) {w.jg.set(0,40,800,600);	} }	
+	}  
+    this.blrSet1	= function(b,d)    {		
+		var tb = blo0.blDiv(d,"id_div_toolbar_function_f2_blrSet1","",blGrey[5]);
+		b.style.background = "gray";
+		var _id = "id_btn_f2_toolbar_blrSet1_b";
 
-	var _blMove = function(o,x,y){o.style.left = x;o.style.top = y;};
-	function _on_off_div(b,d){if(d.style.display=="block"){d.style.display="none"; b.style.backgroundColor="red"; }else{d.style.display="block"; b.style.backgroundColor="green"; }};
-	function _on_off_bd_1(b,d){ 
-		if(b.bOnOff){
-			b.bOnOff = false;
-			b.style.backgroundColor="green"; 
-		}
-		else{
-			b.bOnOff = true;
-			b.style.backgroundColor="red"; 
-		}
-		if(d.style.display=="block"){
-			d.style.display="none"; 
-		}
-		else{
-			d.style.display="block";
-		}
-	}
-	function _on_off_bd(b,d){  
-		if(b.innerHTML=="+"){
-			b.innerHTML = "-";
-			b.style.backgroundColor="green"; 
-		}
-		else{
-			b.innerHTML = "+";
-			b.style.backgroundColor="red"; 
-		}
-		if(d.style.display=="block"){
-			d.style.display="none"; 
-		}
-		else{
-			d.style.display="block";
-		}
-	}
-	function _getXY(){
-			
-			var scrlY =document.body.scrollTop;
-			var scrlX =document.body.scrollLeft;
-			var r = {};
-			r.x=event.clientX+scrlX;
-			r.y=event.clientY+scrlY; 
-			return r;
-	}
-
-function blClass ()
-{ 
-	var _id = "id_div_4_blClass";
-    this.v = "xdvc6: 1.0.80";
-	function _blhMakeLink(txt,href,style,target){
-		var r = "";
-		r += "<a href='" + href + "' ";
-		r += " style=" + style;
-		r += " target=" + target;
-		r +=">" + txt; 
-		r += "</a>";   
-		return r;
-	}
-	this.blhMakeLink = function (txt,href,style,target) { return _blhMakeLink(txt,href,style,target); } 
-    this.blrMax = function (b,d)
-    {        
-		 d.parentElement.parentElement.style.left = 0+"px";
-		 d.parentElement.parentElement.style.top = 0+"px";
-		 d.parentElement.parentElement.style.width = "100%";
-	}
-	this.blrAboutMe= function(b,d){		
-		var s = ""; 
-		s += _blhMakeLink('blclass.js ','https://littleflute.github.io/vc6/XdHtml/blclass.js','color:skyblue;','_blank');
-		s += _blhMakeLink(' blclass.js*','https://github.com/littleflute/vc6/edit/master/XdHtml/blclass.js','color:skyblue;','_blank');
-		d.innerHTML = s;
-		_on_off_div(b,d);
-	}
-	this.blhInitUI		= function(divUI){	
-		var divMove		= blo0.blMDiv(document.body,_id,"divShowMe_divMove_blClass",550,150,500,200,blColor[8]);
-		var dMe			= blo0.blDiv(divMove,divMove.id + "ShowMe","divShowMe",blColor[6]);
-		var dUI			= blo0.blDiv(divUI,divUI.id + "_initUI_blClass" + this.v,this.v,"green");
-		var b1			= blo0.blBtn(dUI,"blhInitUI_btn1","[blClass]",blColor[8]);
-		b1.onclick		= function(this_){	return function(){ 	blo0.blShowObj2Div(dMe,this_);_on_off_div(this,divMove);}}(this);
-		
-	};
-    this.blCreatePage = function (titleTxt,bodyHtml){
-		var r = "";
-		r	+= "<HTML><HEAD><TITLE>";
-		r	+= titleTxt;
-		r	+= "</TITLE></HEAD>";
-		r	+= "<body>";
-		r	+= bodyHtml;
-		r	+= "</body></HTML>";
-
-		return r;
-	};
-    this.blHandle = function (oBoss,id,x,y,w,h,bkClr){
-		var x1 = 0;
-		var y1 = 0; 
-        var r = document.getElementById(id);
-        if(!r){
-            r = document.createElement("div");
-            r.id = id;
-    	    r.innerHTML = ""; 
-            r.style.backgroundColor=bkClr?bkClr:"gold";
-			r.style.position = "absolute";
-			r.style.left		= x+"px";
-			r.style.top			= y+"px";
-			r.style.width		= w+"px";
-			r.style.height		= h+"px";
-			r.style.cursor		= "default";
-    	    if(oBoss!=null)oBoss.appendChild(r);
+		var b1 = blo0.blBtn(tb,_id+1,"b1",blGrey[0]);b1.onclick = function(){
+			var d = blo0.blMDiv(w,"id_div_blrSet1_d1","1:",10,50,100,200,"purple");  
+			_on_off_div(this,d);			
 		}	
-		var divMoveHandle	= this.blDiv(r,id+"divMoveHandle","",bkClr);
-		divMoveHandle.style.cursor		= "move"; 
-		
-		divMoveHandle.style.width		= "100%"; 
-		divMoveHandle.style.height		= "100%"; 
-		var dm = r;
-		divMoveHandle.onmousedown = function(){
-			var c = _getXY(); 
-			x1 = c.x;
-			y1 = c.y;
-			return false;
-		};
-		divMoveHandle.onmousemove = function(){
-			var c = _getXY(); 
-			if(x1==0 &&y1==0) return false;
-
-			var l = dm.style.left;
-			l = parseInt(l);
-			dm.style.left = l + c.x -x1 + "px";
-
-			var t = dm.style.top;
-			t = parseInt(t);
-			dm.style.top = t + c.y -y1 + "px";
-			
-			x1 = c.x;
-			y1 = c.y;
-			return false;
-		};
-		divMoveHandle.onmouseup = function(){
-			var c = _getXY(); 
-			x1 = 0;
-			y1 = 0; 
-		}; 
-		divMoveHandle.onmouseout = function(){
-			var c = _getXY(); 
-			x1 = 0;
-			y1 = 0; 
-		};  
-        return r;
-    }
-    this.blMDiv = function (oBoss,id,html,x,y,w,h,bkClr){
-		var x1 = 0;
-		var y1 = 0; 
-        var r = document.getElementById(id);
-        if(!r){
-            r = document.createElement("div");
-            r.id = id;
-    	    r.innerHTML = html; 
-            r.style.backgroundColor=bkClr?bkClr:"gold";
-			r.style.position = "absolute";
-			r.style.left		= x+"px";
-			r.style.top			= y+"px";
-			r.style.width		= w+"px";
-			r.style.height		= h+"px";
-			r.style.cursor		= "default";
-    	    if(oBoss!=null)oBoss.appendChild(r);
-		}	
-		var divMoveHandle	= this.blDiv(r,id+"divMoveHandle","divMoveHandle","skyblue");
-		divMoveHandle.style.cursor		= "move";
-		var main	= this.blDiv(r,id+"main","main","lightblue");
-		var dm = r;
-		divMoveHandle.onmousedown = function(){
-			var c = _getXY();
-			main.innerHTML = "down:" + c.x + "," + c.y;
-			x1 = c.x;
-			y1 = c.y;
-			return false;
-		};
-		divMoveHandle.onmousemove = function(){
-			var c = _getXY();
-			main.innerHTML = "move:" + c.x + "," + c.y; 
-			if(x1==0 &&y1==0) return false;
-
-			var l = dm.style.left;
-			l = parseInt(l);
-			dm.style.left = l + c.x -x1 + "px";
-
-			var t = dm.style.top;
-			t = parseInt(t);
-			dm.style.top = t + c.y -y1 + "px";
-			
-			x1 = c.x;
-			y1 = c.y;
-			return false;
-		};
-		divMoveHandle.onmouseup = function(){
-			var c = _getXY();
-			main.innerHTML = "up:" + c.x + "," + c.y; 
-			x1 = 0;
-			y1 = 0; 
-		}; 
-		divMoveHandle.onmouseout = function(){
-			var c = _getXY();
-			main.innerHTML = "out:" + c.x + "," + c.y; 
-			x1 = 0;
-			y1 = 0; 
-		};  
-        return r;
-    }
-    _blShowObj2Div_all = function (oBoss,obj,l) //blclassdbg 1039
-    {	  
-        oBoss.innerHTML = "";
-        for(i in obj)
-		{
-			var bOK = i.charAt(0)=="b"&&i.charAt(1)=="l"&&i.charAt(2)=="r"; 
-			if(!bOK) continue;
-
-			  var b = document.createElement("button");
-			  b.id = b.innerHTML = i;
-			  var color = "gray";
-			  if(1==l) color = "blue";
-			  else if( 2==l) color = "pink";
-			  else if( 3==l) color = "red";
-
-			  b.style.backgroundColor = color;
-			  if(i.charAt(0)=="b"&&i.charAt(1)=="l") b.style.backgroundColor = "yellow";
-			  oBoss.appendChild(b);
-			  var d = document.createElement("div");
-			  if("object" == typeof obj[i])
-			  {
-					_blShowObj2Div_all(d,obj[i],l+1);
-			  }
-			  else{
-				d.innerHTML = obj[i];
-			  }
-			  d.style.border = "blue 1px solid";
-			  d.style.backgroundColor = "green";
-			  d.style.color = "yellow";
-			  oBoss.appendChild(d);
-
-			  
-			  if(bOK){
-				b.style.backgroundColor = "red";
-				d.innerHTML = ""; 
-				d.id = b.id + "Div";
-				b.onclick = function(i_,b_,v_,o_){
-					return function(){
-						var go = obj[i_];
-						if (typeof go == "function") {
-			    			go(b_,v_,o_);
-						}
-					} 
-				}(i,b,d,obj);
-			  }
-		}		 
-	}
-    this.blShowObj2Div_all = function (oBoss,obj,l) 
-    {	 
-        _blShowObj2Div_all(oBoss,obj,l);	 
-	}
-    this.blShowObj2Div = function (oDivBoss,obj)
-    {	 
-		_blShowObj2Div(oDivBoss,obj);
-		 
-	}
-    _blShowObj2Div = function (oDivBoss,obj)
-    {        
-        var oBoss = oDivBoss;
-        if(!oBoss) {
-           oBoss = document.createElement("div");
-           oBoss.id = "divBlShowObj";
-           oBoss.style.border = "green 1px solid";
-           document.body.appendChild(oBoss);
-        } 
-        if(!oBoss){
-            alert("boss error!");return;
-        }
-        oBoss.innerHTML = "";
-        for(i in obj)
-		{
-			  var b = document.createElement("button");
-			  b.id = b.innerHTML = i;
-			  b.style.backgroundColor = "gray";
-			  if(i.charAt(0)=="b"&&i.charAt(1)=="l") b.style.backgroundColor = "yellow";
-			  oBoss.appendChild(b);
-			  var d = document.createElement("div");
-			  d.innerHTML = obj[i];
-			  d.style.border = "blue 1px solid";
-			  d.style.backgroundColor = "green";
-			  d.style.color = "yellow";
-			  oBoss.appendChild(d);
-
-			  if(i.charAt(0)=="b"&&i.charAt(1)=="l"&&i.charAt(2)=="r"){
-				b.style.backgroundColor = "red";
-				d.innerHTML = ""; 
-				d.id = b.id + "Div";
-				b.onclick = function(i_,b_,v_){
-					return function(){
-						var go = obj[i_];
-						if (typeof go == "function") {
-			    			go(b_,v_);
-						}
-					} 
-				}(i,b,d);
-			  }
-			  if(i.charAt(0)=="b"&&i.charAt(1)=="l"&&i.charAt(2)=="l"){ 
-					b.style.display = "none";	//v1033	 xdvc6
-					d.id			= d.html;	 
-			  }
-			  if(i.charAt(0)=="b"&&i.charAt(1)=="l"&&i.charAt(2)=="h"){ 
-					b.style.display = "none";	//v1034	 xdvc6	
-					d.style.display = "none";	//v1034	 xdvc6	
-			  }
+		var b2 = blo0.blBtn(tb,_id+2,"b2",blGrey[0]);b2.onclick = function(){
+			var d = blo0.blMDiv(w,"id_div_blrSet1_d2","2:",50,50,100,200,"blue");  
+			_on_off_div(this,d);}	
+		var b3 = blo0.blBtn(tb,_id+3,"b3",blGrey[0]);b3.onclick = function(){ 
+			var d = blo0.blMDiv(w,"id_div_blrSet1_d3","3:",110,50,100,200,"green");  
+			_on_off_div(this,d);}	
+	}  
+    this.blrView	= function(b,d)
+    { 
+		if(w==null){		
+			w = blo0.blMDiv(document.body,"id_div__myWrap","_f2:" + _v,10,50,400,200,"brown"); w.style.zIndex = -99; 
+			w.divView = blo0.blDiv(w,w.id+"divView","divView",blGrey[0]);
+			w.jg		= new _jgClass(w.divView.id);
+			w.jg.upate();
 		}
-    }    
-
-	this.blAudio = function (oBoss,id,src)					
- 	{  								
-   		var s = "";							 
-	   	s += "<";						 
-	   	s += "audio id=";						
-   		s += id;							
-	   	s += " controls>";								 
-	   	s += "<source src='";
-		s += src;
-		s += "' type='audio/mpeg'>";		 
-		s += "Your browser does not supoort hte audio element"				
-	   	s += "</audio>";								 
-	   	oBoss.innerHTML = s;						 
-	 }	
-
-	this.blScript = function (id,src){
-    		var r = document.getElementById(id);
-    		if(!r){
-        		r = document.createElement("script");
-        		r.id = id;
-    		}
-    		r.src = src; 
-    		document.body.appendChild(r);
-    		return r;
-	}
-
-    this.blAjx = function(worker,href)
-    {
-        var xmlhttp;
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200){
-               worker._2do(xmlhttp.responseText);
-            }
-			else{
-              // worker._error("xmlhttp.status:" + xmlhttp.status);
-			}
-        }
-        xmlhttp.open("GET",href,true);
-        xmlhttp.send();
+	  _on_off_div(b,w);
     }
+    this.blr_AboutMe = function(b,d){
+	  var s = _v; 
+	  s += "<br>music staff<br>";  
+	  s += blo0.blhMakeLink(" f2.js ","https://littleflute.github.io/vc6/XdHtml/plx/design/f2.js","color:lightblue;","_blank");
+	  s += blo0.blhMakeLink(" f2.js* ","https://github.com/littleflute/vc6/edit/master/XdHtml/plx/design/f2.js","color:lightblue;","_blank");
+      d.innerHTML = s;  
+	  _on_off_div(b,d);
+    };
+  }
+  function _jgClass(id){
+	var _jg = new jsGraphics(id);
+	var x=0,y=0,w=600,h=400;
+	function _drawAll(){
+		_jg.clear();
+		_jg.setColor("gray");
+		_jg.fillRect(x,y,w,h);
+		_jg.paint();
+	}   
+	this.upate	= function()			{ _drawAll(); }   
+	this.set	= function(x1,y1,w1,h1) { x=x1;y=y1;w=w1;h=h1; _drawAll(); }   
+  }
+   
+  var _myUI = function(b,d){ 
+      _myView	= blo0.blDiv(d,d.id + "_myView","_myView",blGrey[5]);
+	  if(!_myView.blFirst){
+		blo0.blShowObj2Div(_myView,_ui);
+		_myView.blFirst = true;
+		blr_AboutMe.click();
+		blrSet0.click();
+		blrSet1.click();
+	  }
+	  _on_off_div(b,d);
+  };
+  _myUI(b,d);
+};
 
-    this.blDiv = function (oBoss,id,html,bkClr){
-        var r = document.getElementById(id);
-        if(!r){
-            r = document.createElement("div");
-            r.id = id;
-    	    r.innerHTML = html; 
-            r.style.backgroundColor=bkClr?bkClr:"gray";
-    	    if(oBoss!=null)oBoss.appendChild(r);
-        }
-        return r;
-    }
-    this.blTextarea = function (oBoss,id,html,bkClr){
-        var r = document.getElementById(id);
-        if(!r){
-            r = document.createElement("Textarea");
-            r.id = id;
-    	    r.innerHTML = html; 
-            r.style.backgroundColor=bkClr?bkClr:"gray";
-    	    if(oBoss!=null)oBoss.appendChild(r);
-        }
-        return r;
-    }
-
-    this.blBtn = function (oBoss,id,html,bkClr){
-        var r = document.getElementById(id);
-        if(!r){
-            r = document.createElement("button");
-            r.id = id;
-    	    r.innerHTML = html; 
-            r.style.backgroundColor=bkClr?bkClr:"green";
-    	    if(oBoss!=null)oBoss.appendChild(r);
-        }
-        return r;
-    }
-    this.blLink = function (oBoss,id,html,href,bkClr){
-        var r = document.getElementById(id);
-        if(!r){
-            r = document.createElement("a");
-    	    var t = document.createTextNode(html);
-    	    r.setAttribute("href", href);
-            r.id = id; 
-    	    r.style.backgroundColor = bkClr?bkClr:"blue";
-        }
-        r.innerHTML = html + " "; 
-        oBoss.appendChild(r);
-        return r;
-    }
-
-    this.blMakeDivMovable = function (elmnt) {
-      	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-	var idHeader = document.getElementById(elmnt.id + "Header");
-      	if (idHeader) {
-        	/* if present, the header is where you move the DIV from:*/
-        	idHeader.onmousedown = dragMouseDown;
-      	} else {
-        	/* otherwise, move the DIV from anywhere inside the DIV:*/
-        	elmnt.onmousedown = dragMouseDown;
-      	}
-
-      	function dragMouseDown(e) {
-        	e = e || window.event;
-        	// get the mouse cursor position at startup:
-        	pos3 = e.clientX;
-        	pos4 = e.clientY;
-        	document.onmouseup = closeDragElement;
-        	// call a function whenever the cursor moves:
-        	document.onmousemove = elementDrag;
-		if (idHeader) {
-        		idHeader.innerHTML = pos3 + "," + pos4;
-      		}	
-      	}
-
-      function elementDrag(e) {
-        e = e || window.event;
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      }
-
-      function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-      }
-    }
-	
-}//END: function blClass ()
- 
- var blo0 = new blClass;
+var ta = bl$( "id_ta_4_NewClass" );
+if( ta ){
+	ta.f2 = f2;
+	ta.value = ta.f2;
+}
