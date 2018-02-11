dcdedeff95fa828baaf19d21098c6127dbb11031
littleflute <13699175164@sina.cn>
2018-02-11 16:03:12 +0800

    Create zw1_1.js

---
+++ XdHtml/cchess/pu/zw1_1.js
@@ -0,0 +1,17 @@
+var f1 = function(){
+  //f1.js * 佐為象棋講座-仙人指路
+  //https://www.youtube.com/watch?v=SxJWB2ryVzE
+  //第一系统
+
+  var _v			= "v0.0.53";
+  var s = "C3-C4+B7-C7+H2-E2+";
+
+ 
+  return s;
+};
+
+var ta = bl$( "id_ta_4_puList" );
+if( ta ){
+	ta.f1 = f1;
+	ta.value = ta.f1;
+}
