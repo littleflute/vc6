a698105a5da18074da615d876c8a212e189f8530
littleflute <13699175164@sina.cn>
2018-02-11 16:13:05 +0800

    Update zw1_2.js

--- XdHtml/cchess/pu/zw1_2.js
+++ XdHtml/cchess/pu/zw1_2.js
@@ -1,9 +1,11 @@
 var f1 = function(){
   //f1.js * 
-  //https://www.youtube.com/watch?v=BF2Sf0ehCWM
+  //https://www.youtube.com/watch?v=SxJWB2ryVzE
+//佐為象棋講座-仙人指路 2：47
 
-  var _v			= "v0.0.51";
-  var s = "H2-D2+H7-E7+H0-G2+H9-G7+I0-H0+I9-I8+H0-H6+I8-D8+D0-E1+B9-C7+H6-G6+E6-E5+C0-E2+C7-E6+B0-A2+E5-E4+E3-E4+E7-E4+D2-C2+B7-E7+G3-G4+A9-B9+A0-B0+E6-D4+C2-D2+D4-E2+B2-E2+B9-B0+A2-B0+D8-D2+G6-G7+D9-E8+";
+
+  var _v			= "v0.0.52";
+  var s = "C3-C4+B7-C7+B2-E2+H9-G7+B0-C2+";
 
  
   return s;
